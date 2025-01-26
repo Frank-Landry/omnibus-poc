301bab940e45e9254afe4c6f99280da4b78a6140
Frank-Landry <franklandry85@gmail.com>
2025-01-25 22:50:40 -0500

    initial commit

---
+++ dcf_payments_distribtion/tests/unit/TestReceivedPaymentEvent.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\dcf_payment_distribution\tests\unit;
+
+use PHPUnit\Framework\TestCase;
+
+class TestReceivedPaymentEvent extends TestCase {
+
+  public function test_it_works()
+  {
+    $this->assertTrue(true);
+  }
+
+}
